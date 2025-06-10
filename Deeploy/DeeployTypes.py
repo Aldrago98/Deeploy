ca8df232b4d0e92b6b357b35925864a446b1699f
Aldrago98 <67410630+Aldrago98@users.noreply.github.com>
2025-06-10 12:27:27 +0000

    Floating point support for 1D convolution padding

--- Deeploy/DeeployTypes.py
+++ Deeploy/DeeployTypes.py
@@ -1588,7 +1588,7 @@ class NodeBinding():
         
         """
 
-        print(f"[DEBUG] NodeBinding: Using typeChecker: {type(self.typeChecker)} for node {node.name}")
+        print(f"[DEBUG] NodeBinding: Using typeChecker: {self.typeChecker} for node {node.name}")
         newCtxt, ret = self.typeChecker.typeCheck(ctxt.copy(), node, operatorRepresentation)
         print(f'value of ret inside typeCheck  of NodeBinding {ret}')
         if ret:
