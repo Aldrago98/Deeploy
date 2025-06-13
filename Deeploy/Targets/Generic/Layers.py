979db2970fd5d6a7f6a31de4a1c6b1e7ac2df584
Aldrago98 <67410630+Aldrago98@users.noreply.github.com>
2025-06-13 14:59:04 +0000

    Add base class for ConvTranspose

--- Deeploy/Targets/Generic/Layers.py
+++ Deeploy/Targets/Generic/Layers.py
@@ -683,6 +683,7 @@ class ConvTransposeLayer(ONNXLayer):
                 newOutputShapes[0][-1] = ch_out
 
         return newInputShapes, newOutputShapes
+
     def computeOps(self):
         opRep = self.mapper.parser.operatorRepresentation
 
