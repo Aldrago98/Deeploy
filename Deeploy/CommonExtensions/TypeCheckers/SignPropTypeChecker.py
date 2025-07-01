21ca4fd07dc9d21bc23e2b54bfce62b987304d29
Aldrago98 <67410630+Aldrago98@users.noreply.github.com>
2025-07-01 15:42:30 +0000

    Removed Italian comments and update changelog.md

--- Deeploy/CommonExtensions/TypeCheckers/SignPropTypeChecker.py
+++ Deeploy/CommonExtensions/TypeCheckers/SignPropTypeChecker.py
@@ -67,8 +67,8 @@ class SignPropTypeChecker(NodeTypeChecker):
             nLevels = self._inferNumLevels(inputs, operatorRepresentation)
             signedness = self._inferSignedness(inputs, operatorRepresentation)
 
-            for obj, nl, sign in zip(outputs, nLevels, signedness):
-                obj.nLevels = nl
+            for obj, nLevels, sign in zip(outputs, nLevels, signedness):
+                obj.nLevels = nLevels
                 obj._signed = sign
 
         return ctxt
